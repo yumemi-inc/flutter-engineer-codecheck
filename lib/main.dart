2db7e27c4bdac3278690d9dcf8d30a10552caaba
RYO1223 <kuwanor0811@gmail.com>
2023-11-09 12:15:10 +0900

    fix: riverpod initialization bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
