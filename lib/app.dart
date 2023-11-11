ce86c382458586222e0299b720b276bbc4a4f348
RYO1223 <kuwanor0811@gmail.com>
2023-11-11 12:23:49 +0900

    feat: ダークテーマを追加

--- lib/app.dart
+++ lib/app.dart
@@ -3,17 +3,21 @@ import 'package:flutter_engineer_codecheck/ui/repo_search/repo_search_page.dart'
 import 'package:flutter_engineer_codecheck/ui/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// TODO(kuwano): バックグラウンドでのライト・ダークテーマ切り替えについて調べる
+// Twitter, Line, Settingのようなアプリはバックグラウンドで切り替えられるが、
+// Flutterでは解決策はないようだ。
+// ref: https://github.com/flutter/flutter/issues/43806
 class App extends ConsumerWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appTheme = ref.watch(appThemeProvider);
 
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: appTheme,
+      theme: appTheme.light(),
+      darkTheme: appTheme.dark(),
       home: const RepoSearchPage(),
     );
   }
